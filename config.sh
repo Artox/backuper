dfea9a50cfd05a95c8694ce441c275b723ee08d0
Artox <artox@hellgate-wow.net>
2012-05-27 17:28:57 +0200

    Now also files can be backed up

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # config file for backups
 
+# backup location
 backupdir=/opt/backups
 
 # which databases to back up
@@ -13,6 +14,11 @@ backupdir=/opt/backups
 # be sure to set database_count correct (to the highest i)
 database_count=1
 database_1=mydb
-database_1_host=127.0.0.1
+database_1_host=myhost
 database_1_user=myuser
 database_1_pass=mypass
+
+# which files to back up
+#
+file_count=1
+file_1=<yourfile>
--- run.sh
+++ run.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
+# compresses a file with gzip
+# params: <resultvar> <file>
+compress_file(){
+	# retrieve parameters
+	result=$1
+	file=$2
+
+	# compress
+	gzip "$file"
+
+	eval $result="'$file.gz'"
+}
+
+# backs up file by copying/moving it to the backup location
+# params: <file> <backuppath> <keep = true>
+backup_file(){
+	# retrieve parameters
+	file=$1
+	backupdir=$2
+	if [ "$3" ]; then keep=$3; else keep=true; fi
+
+	# put file where it belongs
+	if $keep
+	then
+		cp "$file" "$backupdir/"
+	else
+		mv "$file" "$backupdir/"
+	fi
+}
+
 # makes dumpfile of database
 # params: <resultvar> <dbhost> <dbuser> <dbpass> <dbname>
 create_database_dump(){
@@ -24,19 +54,6 @@ create_database_dump(){
 	eval $result="'$dumpfile'"
 }
 
-# compresses a file with gzip
-# params: <resultvar> <file>
-compress_file(){
-	# retrieve parameters
-	result=$1
-	file=$2
-
-	# compress
-	gzip "$file"
-
-	eval $result="'$file.gz'"
-}
-
 # will loop through all given databses in config file and back them up
 # params: <dbcount> <backuppath>
 backup_databases(){
@@ -56,12 +73,41 @@ backup_databases(){
 		# perform the backup
 		create_database_dump dumpfile "$host" "$user" "$pass" "$db"
 		compress_file dumpfile "$dumpfile"
-		mv "$dumpfile" "$backupdir/"
+		backup_file "$dumpfile" "$backupdir" false
 
 		# done
 		echo "Created $dumpfile"
 	done
 }
+
+# will loop through all files lsited to be backed up
+# params: <filecount> <backuppath>
+backup_files(){
+	# retrieve parameters
+	count=$1
+	backupdir=$2
+
+	# act on each file
+	for i in `seq 1 $count`
+	do
+		# get name of file i
+		eval file=\$file_$i
+
+		# append current timestamp to filename
+		filename=`basename $file`
+		timestamp=`date +"%g-%m-%d %H:%M:%S"`
+		filename="$filename $timestamp"
+
+		# perform the backup
+		cp "$file" "./$filename"
+		compress_file backup "$filename"
+		backup_file "$backup" "$backupdir" false
+
+		# done
+		echo "Created $backup"
+	done
+}
+
 
 # here the main work is done
 # load configuration
@@ -69,3 +115,6 @@ source ./config.sh
 
 # backup databases
 backup_databases $database_count "$backupdir"
+
+# backup files
+backup_files $file_count "$backupdir"
